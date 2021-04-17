1b4cdd4541dcfbb9313b0a783ad774833d2d41f5
Anto Sujesh <sujeshantoj@gmail.com>
2021-04-17 22:05:06 +0530

    Add files via upload

---
+++ ConsoleApp3/AutoSQL.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Data;
+
+namespace AutoExe
+{
+    class AutoSQL
+    {
+
+        public bool RunSqlScriptFile(string pathStoreProceduresFile,string ID, string connectionString)
+        {
+            try
+            {
+
+                string script = File.ReadAllText(pathStoreProceduresFile);
+
+                // split script on GO command
+                System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
+                                         RegexOptions.Multiline | RegexOptions.IgnoreCase);
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    foreach (string commandString in commandStrings)
+                    {
+                        if (commandString.Trim() != "")
+                        {
+                            using (var command = new SqlCommand(commandString, connection))
+                            {
+                                try
+                                {
+                                    command.ExecuteNonQuery();
+                                }
+                                catch (SqlException ex)
+                                {
+                                    string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
+                                    string RESP = string.Format("Please check your SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathStoreProceduresFile, ex.LineNumber, ex.Message, spError);
+                                    Console.Write(RESP); Console.Write("\n\n\nSCRIPT EXE FAILED");
+                                    UpdateStatus(connectionString, "FAILED", RESP,ID); 
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+                UpdateStatus(connectionString, "SUCCESS", "SCRIPT EXE SUCCESSFULLY ", ID);
+                Console.Write("SCRIPT EXE SUCCESSFULLY");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+                UpdateStatus(connectionString, "FAILED", ex.Message, ID);
+                Console.Write("SCRIPT EXE FAILED");
+                return false;
+            }
+        }
+
+        public void Loop_Pending_Script(string Connection, string SQL, string ScriptPath) //ScriptPath
+        {
+            DataTable dt = GetDataTable(Connection, SQL);
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    RunSqlScriptFile(ScriptPath + dt.Rows[i]["FILE_NAME"].ToString(), dt.Rows[i]["ID"].ToString(), Connection);
+                }
+            }
+        }
+
+
+        public static DataTable GetDataTable(string connection, string sql)
+        {
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        public void UpdateStatus(string Connection, string status,string Response,  string ID)
+        {
+            SqlConnection connection = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand("update EXE_SCRIPT set STATUS=@STATUS ,RESPONSE=@RESPONSE where ID=@ID ", connection);
+            cmd.Parameters.AddWithValue("@STATUS", status);
+            cmd.Parameters.AddWithValue("@RESPONSE", Response);
+            cmd.Parameters.AddWithValue("@ID", ID);
+            cmd.CommandType = CommandType.Text;
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void InsertRecord(string Connection, string USER_NAME, string PASSWORD, string CONTACT_NUMBER, string EMAIL, string Code)
+        {
+            SqlConnection connection = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand("insert into Oman (USER_NAME,PASSWORD,CONTACT_NUMBER,EMAIL,Code) values (@USER_NAME,@PASSWORD,@CONTACT_NUMBER,@EMAIL,@Code) ", connection);
+            cmd.Parameters.AddWithValue("@USER_NAME", USER_NAME);
+            cmd.Parameters.AddWithValue("@PASSWORD", PASSWORD);
+            cmd.Parameters.AddWithValue("@CONTACT_NUMBER", CONTACT_NUMBER);
+            cmd.Parameters.AddWithValue("@EMAIL", EMAIL);
+            cmd.Parameters.AddWithValue("@Code", Code);
+            cmd.CommandType = CommandType.Text;
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+        public void UpdateRecord(string Connection, string USER_NAME, string CONTACT_NUMBER, string EMAIL, string Code)
+        {
+            SqlConnection connection = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand("update Oman set Code =@Code where (CONTACT_NUMBER=@CONTACT_NUMBER and EMAIL=@EMAIL) or CONTACT_NUMBER=@CONTACT_NUMBER or EMAIL=@EMAIL ", connection);
+            cmd.Parameters.AddWithValue("@CONTACT_NUMBER", CONTACT_NUMBER);
+            cmd.Parameters.AddWithValue("@EMAIL", EMAIL);
+            cmd.Parameters.AddWithValue("@Code", Code);
+            cmd.CommandType = CommandType.Text;
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+
+
+    }
+}
---
+++ ConsoleApp3/EinvoiceModel.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace AutoExe
+{
+    class EinvoiceModel
+    {
+
+    }
+    public partial class EInvoice
+    {
+        [JsonProperty("Version")]
+        public string Version { get; set; }
+
+        [JsonProperty("TranDtls")]
+        public TranDtls TranDtls { get; set; }
+
+        [JsonProperty("DocDtls")]
+        public DocDtls DocDtls { get; set; }
+
+        [JsonProperty("SellerDtls")]
+        public Seller_Dtls SellerDtls { get; set; }
+
+        [JsonProperty("BuyerDtls")]
+        public Buyer_Dtls BuyerDtls { get; set; }
+
+        [JsonProperty("DispDtls")]
+        public Disp_Dtls DispDtls { get; set; }
+
+
+        [JsonProperty("ShipDtls")]
+        public Shipping_Dtls ShipDtls { get; set; }
+
+       
+
+   
+
+        [JsonProperty("ValDtls")]
+        public ValDtls ValDtls { get; set; }
+
+        [JsonProperty("ExpDtls")]
+        public ExpDtls ExpDtls { get; set; }
+
+        [JsonProperty("EwbDtls")]
+        public EwbDtls EwbDtls { get; set; }
+
+        [JsonProperty("ItemList")]
+        public ItemList[] ItemList { get; set; }
+    }
+    public class ValDtls
+    {
+        public decimal? AssVal { get; set; }
+        public decimal? IgstVal { get; set; }
+        public decimal? CgstVal { get; set; }
+        public decimal? SgstVal { get; set; }
+        public decimal? CesVal { get; set; }
+        public decimal? StCesVal { get; set; }
+        public decimal? Discount { get; set; }
+        public decimal? OthChrg { get; set; }
+        public decimal? RndOffAmt { get; set; }
+        public decimal? TotInvVal { get; set; }
+    }
+    public partial class Buyer_Dtls
+    {
+        [JsonProperty("Gstin")]
+        public string Gstin { get; set; }
+
+        [JsonProperty("LglNm")]
+        public string LglNm { get; set; }
+
+        [JsonProperty("TrdNm")]
+        public object TrdNm { get; set; }
+
+        [JsonProperty("Pos")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public string Pos { get; set; }
+
+        [JsonProperty("Addr1")]
+        public string Addr1 { get; set; }
+
+        [JsonProperty("Addr2")]
+        public object Addr2 { get; set; }
+
+        [JsonProperty("Loc")]
+        public string Loc { get; set; }
+
+        [JsonProperty("Pin")]
+        public long Pin { get; set; }
+
+        [JsonProperty("Stcd")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public object Stcd { get; set; }
+
+        [JsonProperty("Ph")]
+        public object Ph { get; set; }
+
+        [JsonProperty("Em")]
+        public object Em { get; set; }
+    }
+    public partial class Disp_Dtls
+    {
+        [JsonProperty("Nm")]
+        public string Nm { get; set; }
+
+       
+        [JsonProperty("Addr1")]
+        public string Addr1 { get; set; }
+
+        [JsonProperty("Addr2")]
+        public object Addr2 { get; set; }
+
+        [JsonProperty("Loc")]
+        public string Loc { get; set; }
+
+        [JsonProperty("Pin")]
+        public long Pin { get; set; }
+
+        [JsonProperty("Stcd")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public object Stcd { get; set; }
+
+    }
+    
+    public partial class Shipping_Dtls
+    {
+        [JsonProperty("Gstin")]
+        public string Gstin { get; set; }
+
+        [JsonProperty("LglNm")]
+        public string LglNm { get; set; }
+
+        [JsonProperty("TrdNm")]
+        public object TrdNm { get; set; }
+
+
+        [JsonProperty("Addr1")]
+        public string Addr1 { get; set; }
+
+        [JsonProperty("Addr2")]
+        public object Addr2 { get; set; }
+
+        [JsonProperty("Loc")]
+        public string Loc { get; set; }
+
+        [JsonProperty("Pin")]
+        public int Pin { get; set; }
+
+        [JsonProperty("Stcd")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public object Stcd { get; set; }
+
+      
+    }
+    public partial class Seller_Dtls
+    {
+        [JsonProperty("Gstin")]
+        public string Gstin { get; set; }
+
+        [JsonProperty("LglNm")]
+        public string LglNm { get; set; }
+
+        [JsonProperty("TrdNm")]
+        public object TrdNm { get; set; }
+
+      
+        [JsonProperty("Addr1")]
+        public string Addr1 { get; set; }
+
+        [JsonProperty("Addr2")]
+        public object Addr2 { get; set; }
+
+        [JsonProperty("Loc")]
+        public string Loc { get; set; }
+
+        [JsonProperty("Pin")]
+        public int Pin { get; set; }
+
+        [JsonProperty("Stcd")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public object Stcd { get; set; }
+
+        [JsonProperty("Ph")]
+        public object Ph { get; set; }
+
+        [JsonProperty("Em")]
+        public object Em { get; set; }
+    }
+
+    public partial class DocDtls
+    {
+        [JsonProperty("Typ")]
+        public string Typ { get; set; }
+
+        [JsonProperty("No")]
+        public string No { get; set; }
+
+        [JsonProperty("Dt")]
+        public string Dt { get; set; }
+    }
+
+    public partial class ExpDtls
+    {
+        [JsonProperty("ShipBNo")]
+        public object ShipBNo { get; set; }
+
+        [JsonProperty("ShipBDt")]
+        public object ShipBDt { get; set; }
+
+        [JsonProperty("Port")]
+        public object Port { get; set; }
+
+        [JsonProperty("RefClm")]
+        public object RefClm { get; set; }
+
+        [JsonProperty("ForCur")]
+        public object ForCur { get; set; }
+
+        [JsonProperty("CntCode")]
+        public object CntCode { get; set; }
+
+        [JsonProperty("ExpDuty")]
+        public decimal ExpDuty { get; set; }
+    }
+
+    public partial class ItemList
+    {
+        [JsonProperty("SlNo")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public string SlNo { get; set; }
+
+        [JsonProperty("PrdDesc")]
+        public string PrdDesc { get; set; }
+
+        [JsonProperty("IsServc")]
+        public string IsServc { get; set; }
+
+        [JsonProperty("HsnCd")]
+        //[JsonConverter(typeof(ParseStringConverter))]
+        public string HsnCd { get; set; }
+
+        [JsonProperty("Qty")]
+        public decimal Qty { get; set; }
+
+        [JsonProperty("Unit")]
+        public string Unit { get; set; }
+
+        [JsonProperty("UnitPrice")]
+        public decimal UnitPrice { get; set; }
+
+        [JsonProperty("TotAmt")]
+        public decimal TotAmt { get; set; }
+
+        [JsonProperty("Discount")]
+        public decimal Discount { get; set; }
+
+        [JsonProperty("PreTaxVal")]
+        public decimal PreTaxVal { get; set; }
+
+        [JsonProperty("AssAmt")]
+        public decimal AssAmt { get; set; }
+
+        [JsonProperty("GstRt")]
+        public decimal GstRt { get; set; }
+
+        [JsonProperty("IgstAmt")]
+        public decimal IgstAmt { get; set; }
+
+        [JsonProperty("CgstAmt")]
+        public decimal CgstAmt { get; set; }
+
+        [JsonProperty("SgstAmt")]
+        public decimal SgstAmt { get; set; }
+
+        [JsonProperty("CesRt")]
+        public decimal CesRt { get; set; }
+
+        [JsonProperty("CesAmt")]
+        public decimal CesAmt { get; set; }
+
+        [JsonProperty("CesNonAdvlAmt")]
+        public decimal CesNonAdvlAmt { get; set; }
+
+        [JsonProperty("StateCesRt")]
+        public decimal StateCesRt { get; set; }
+
+        [JsonProperty("StateCesAmt")]
+        public decimal StateCesAmt { get; set; }
+
+        [JsonProperty("StateCesNonAdvlAmt")]
+        public decimal StateCesNonAdvlAmt { get; set; }
+
+        [JsonProperty("OthChrg")]
+        public decimal OthChrg { get; set; }
+
+        [JsonProperty("TotItemVal")]
+        public decimal TotItemVal { get; set; }
+
+        [JsonProperty("BchDtls")]
+        public BchDtls BchDtls { get; set; }
+    }
+
+    public partial class BchDtls
+    {
+        [JsonProperty("Nm")]
+        public string Nm { get; set; }
+
+        [JsonProperty("ExpDt")]
+        public string ExpDt { get; set; }
+
+        [JsonProperty("WrDt")]
+        public string WrDt { get; set; }
+    }
+
+    public partial class TranDtls
+    {
+        [JsonProperty("TaxSch")]
+        public string TaxSch { get; set; }
+
+        [JsonProperty("SupTyp")]
+        public string SupTyp { get; set; }
+
+        [JsonProperty("IgstOnIntra")]
+        public string IgstOnIntra { get; set; }
+
+        [JsonProperty("RegRev")]
+        public object RegRev { get; set; }
+
+        [JsonProperty("EcmGstin")]
+        public object EcmGstin { get; set; }
+    }
+
+
+    public class EwbDtls
+    {
+        public string TransId { get; set; }
+        public string TransName { get; set; }
+        public string TransMode { get; set; }
+        public decimal Distance { get; set; }
+        public string TransDocNo { get; set; }
+        public string TransDocDt { get; set; }
+        public string VehNo { get; set; }
+        public string VehType { get; set; }
+    }
+
+
+    public class JsonoutRoot
+    {
+        public long AckNo { get; set; }
+        public string AckDt { get; set; }
+        public string Irn { get; set; }
+        public string SignedInvoice { get; set; }
+        public string SignedQRCode { get; set; }
+        public string Status { get; set; }
+        public object EwbNo { get; set; }
+        public object EwbDt { get; set; }
+        public object EwbValidTill { get; set; }
+        public object Remarks { get; set; }
+    }
+
+
+    public class ExcelOut
+    {
+        public string ExInvNo { get; set; }
+        public string AckNo { get; set; }
+        public string AckDt { get; set; }
+        public string Irn { get; set; }
+        public string QRImage { get; set; }
+        public string SignedQRCode { get; set; }
+        public string Status { get; set; }
+    }
+
+}
